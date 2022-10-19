cc76086f9b08d423c31edd7afc8957981da2fc97
lwhyljx <66722751+lwhyljx@users.noreply.github.com>
2022-10-19 13:50:57 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -5,7 +5,11 @@ export ANDROID_NDK=/home/ljx/Android/Sdk/ndk/20b
 rm -r build
 mkdir build && cd build
 
-cmake  ..
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
+    -DANDROID_ABI="arm64-v8a" \
+    -DANDROID_NDK=$ANDROID_NDK \
+    -DANDROID_PLATFORM=android-24 \
+    ..
 
 make && make install
 
